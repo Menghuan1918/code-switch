3a78b7b19628d865d0230bf48187caaa811cde61
Menghuan1918 <menghuan2003@outlook.com>
2025-12-03 17:20:10 +0800

    fix: 移除不可用的打包功能以及清理安装脚本

--- build/linux/appimage/build.sh
+++ build/linux/appimage/build.sh
@@ -14,34 +14,23 @@ cp -r "${APP_BINARY}" "${APP_DIR}/usr/bin/"
 cp "${ICON_PATH}" "${APP_DIR}/"
 cp "${DESKTOP_FILE}" "${APP_DIR}/"
 
-# 复制 WebKit2GTK 辅助进程
-echo "Copying WebKit2GTK auxiliary processes..."
-WEBKIT_LIB_DIR="/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1"
-if [ -d "${WEBKIT_LIB_DIR}" ]; then
-    mkdir -p "${APP_DIR}/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1"
-    cp -r "${WEBKIT_LIB_DIR}"/* "${APP_DIR}/usr/lib/x86_64-linux-gnu/webkit2gtk-4.1/"
-    echo "✓ WebKit auxiliary processes copied"
-else
-    echo "⚠ Warning: WebKit library directory not found at ${WEBKIT_LIB_DIR}"
-    echo "  AppImage may fail to run if WebKit is required"
-fi
-
-if [[ $(uname -m) == *x86_64* ]]; then
-    # Download linuxdeploy and make it executable
-    wget -q -4 -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-    chmod +x linuxdeploy-x86_64.AppImage
-
-    # Run linuxdeploy to bundle the application
-    ./linuxdeploy-x86_64.AppImage --appdir "${APP_DIR}" --output appimage
-else
-    # Download linuxdeploy and make it executable (arm64)
-    wget -q -4 -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-aarch64.AppImage
-    chmod +x linuxdeploy-aarch64.AppImage
-
-    # Run linuxdeploy to bundle the application (arm64)
-    ./linuxdeploy-aarch64.AppImage --appdir "${APP_DIR}" --output appimage
-fi
-
-# Rename the generated AppImage
-mv "${APP_NAME}*.AppImage" "${APP_NAME}.AppImage"
+# 疑似 Wails 上游 bug，AppImage 打包出来无法使用
+# if [[ $(uname -m) == *x86_64* ]]; then
+#     # Download linuxdeploy and make it executable
+#     wget -q -4 -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+#     chmod +x linuxdeploy-x86_64.AppImage
+#
+#     # Run linuxdeploy to bundle the application
+#     ./linuxdeploy-x86_64.AppImage --appdir "${APP_DIR}" --output appimage
+# else
+#     # Download linuxdeploy and make it executable (arm64)
+#     wget -q -4 -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-aarch64.AppImage
+#     chmod +x linuxdeploy-aarch64.AppImage
+#
+#     # Run linuxdeploy to bundle the application (arm64)
+#     ./linuxdeploy-aarch64.AppImage --appdir "${APP_DIR}" --output appimage
+# fi
+#
+# # Rename the generated AppImage
+# mv "${APP_NAME}*.AppImage" "${APP_NAME}.AppImage"
 
